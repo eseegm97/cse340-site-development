1d86fb1a59407871a8e9c6435c23f12e26aaac05
eseegm97 <eseegm97@gmail.com>
2025-10-07 19:08:47 -0500

    classification_view

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express()
 const static = require("./routes/static")
 const expressLayouts = require("express-ejs-layouts")
 const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
