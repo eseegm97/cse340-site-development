a366170ce52ead079d242522e151049b24d74bac
eseegm97 <eseegm97@gmail.com>
2025-12-17 21:17:41 -0600

    fix_index.js

--- database/index.js
+++ database/index.js
@@ -13,25 +13,28 @@ if (process.env.NODE_ENV == "development") {
     ssl: {
       rejectUnauthorized: false,
     },
-})
-
-// Added for troubleshooting queries
-// during development
-module.exports = {
-  async query(text, params) {
-    try {
-      const res = await pool.query(text, params)
-      console.log("executed query", { text })
-      return res
-    } catch (error) {
-      console.error("error in query", { text })
-      throw error
-    }
-  },
-}
+  })
 } else {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
   })
+}
+
+// Added for troubleshooting queries
+// during development
+if (process.env.NODE_ENV == "development") {
+  module.exports = {
+    async query(text, params) {
+      try {
+        const res = await pool.query(text, params)
+        console.log("executed query", { text })
+        return res
+      } catch (error) {
+        console.error("error in query", { text })
+        throw error
+      }
+    },
+  }
+} else {
   module.exports = pool
 }
